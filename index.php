2cba5bcc9fd784cb8182ece936f963a813afad6b
AmirMahdiEsmaili <mr.amirmahdiesmaili@gmail.com>
2024-06-18 06:44:02 -0700

    initail commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('header.php'); ?>
+<?php include('Navbar.php'); ?>
+<?php include('Food_Search.php'); ?>
+<?php include('Categories.php'); ?>
+<?php include('food_Menu.php'); ?>
+<?php include('social.php'); ?>
+<?php include('footer.php'); ?>
